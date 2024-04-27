d2f470d84b1e3ee0c3671d7e6b95dd560983b99f
paulobonifacio <paulo.bonifacio@live.com>
2024-04-27 16:52:37 +0200

    Add the logic to play the list game

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+       List<int> numbers = new List<int>();
+       Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        int input;
+        do
+        {
+            Console.Write("Enter number: ");
+            input = int.Parse(Console.ReadLine());
+
+            if (input != 0)
+            {
+                numbers.Add(input);
+            }
+
+        } while (input != 0);
+
+        if (numbers.Count == 0)
+        {
+            Console.WriteLine("No numbers entered.");
+            return;
+        }
+
+        // Sum the values.
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+
+        // Calculate the average.
+        double average = (double)sum / numbers.Count;
+
+        // Identify the maximum number.
+        int max = numbers[0];
+        foreach (int num in numbers)
+        {
+            if (num > max)
+            {
+                max = num;
+            }
+        }
+
+        // Show the Output messages.
+        Console.WriteLine("The sum is: " + sum);
+        Console.WriteLine("The average is: " + average);
+        Console.WriteLine("The largest number is: " + max);
     }
 }
