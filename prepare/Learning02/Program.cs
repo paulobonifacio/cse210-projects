ffbfbe7f3ed821d257d3f34dbb781bb0c9a7264d
paulobonifacio <paulo.bonifacio@live.com>
2024-05-01 18:26:53 +0200

    W2 Abstraction Learning Activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2015;
+        job1._endYear = 2018;
+
+
+        Job job2 = new Job();
+        job2._jobTitle = "Consultant";
+        job2._company = "IBM";
+        job2._startYear = 2018;
+        job2._endYear = 2024;
+
+        job1.Display();
+        job2.Display();
+        Console.WriteLine();
+
+        Resume myResume = new Resume();
+        myResume._name = "Paulo B";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
